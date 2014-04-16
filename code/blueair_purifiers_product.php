cf37c8dffa61783297013efa4e70585c1b5825f6
zhao <keith_cn@hotmail.com>
2014-04-16 21:50:14 +0800

    blueair_purifiers_other_product.php

--- code/blueair_purifiers_product.php
+++ code/blueair_purifiers_product.php
@@ -65,7 +65,7 @@ $get_model=mysql_fetch_array($sql_model);
 <? $sql_serie=mysql_query("select * from tbl_category where published=1 order by sort_id");
 while($series=mysql_fetch_array($sql_serie)){?>
 <li><a href="blueair_purifiers_series.php?serieid=<?=$series['cate_id'];?>" title="<?=$series['category_name'];?>" <? if($series['cate_id']==$_GET['serieid']){?> class="active"<? }?>><?=$series['category_name'];?></a>
-<? $sql_model=mysql_query("select * from tbl_products where cateid='".$series['cate_id']."' and published=1 order by id");
+<? $sql_model=mysql_query("select * from tbl_products where cateid='".$series['cate_id']."' and published=1 and lang = 1 order by id");
 $num_model=mysql_num_rows($sql_model);
 if($num_model){?>
 <div class="menu1" style="display:<? if($series['cate_id']==$_GET['serieid']){echo "inline";}else{ echo "none";}?>">
--- code/blueair_purifiers_series.php
+++ code/blueair_purifiers_series.php
@@ -33,7 +33,7 @@ $get_serie=mysql_fetch_array($sqlserie);
 <!-- ///////////////////////Body Part Start Here /////////////////////////-->
 <div id="bodysubContainer">
 <div class="bodyleftContainer"><h1><?=$get_serie['category_name'];?></h1><?=$get_serie['description'];?><br /><br />
-<? $sqlmodel=mysql_query("select * from tbl_products where cateid='".$get_serie['cate_id']."' and published=1 order by id");
+<? $sqlmodel=mysql_query("select * from tbl_products where cateid='".$get_serie['cate_id']."' and published=1 and lang = 1 order by id");
 $nummodel=mysql_num_rows($sqlmodel);
 if($nummodel){ $p=1;while($getmodel=mysql_fetch_array($sqlmodel)){?>
 <div class="product_left" <? if($p%2==0){?> style="margin-left:35px; border-left: #CCCCCC solid 1px; padding:0 0 0 35px;"<? }?>>
@@ -46,10 +46,10 @@ if($nummodel){ $p=1;while($getmodel=mysql_fetch_array($sqlmodel)){?>
 <h1>Featured products</h1>
 <div class="menu">
 <ul>
-<? $sql_serie=mysql_query("select * from tbl_category where published=1 order by sort_id");
+<? $sql_serie=mysql_query("select * from tbl_category where published=1   order by sort_id");
 while($series=mysql_fetch_array($sql_serie)){?>
 <li><a href="blueair_purifiers_series.php?serieid=<?=$series['cate_id'];?>" title="<?=$series['category_name'];?>" <? if($series['cate_id']==$_GET['serieid']){?> class="active"<? }?>><?=$series['category_name'];?></a>
-<? $sql_model=mysql_query("select * from tbl_products where cateid='".$series['cate_id']."' and published=1 order by id");
+<? $sql_model=mysql_query("select * from tbl_products where cateid='".$series['cate_id']."' and lang = 1  and published=1 order by id");
 $num_model=mysql_num_rows($sql_model);
 if($num_model){?>
 <div class="menu1" style="display:<? if($series['cate_id']==$_GET['serieid']){echo "inline";}else{ echo "none";}?>">
