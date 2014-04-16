4a87c468983be1d5f0c16ceb237a870b5f612af0
zhao <keith_cn@hotmail.com>
2014-04-16 21:55:08 +0800

    local db connection

--- code/codelibrary/connection.php
+++ code/codelibrary/connection.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
-mysql_connect("localhost","airclean_Manoj","epAeAZzfA8s~");
+//mysql_connect("localhost","airclean_Manoj","epAeAZzfA8s~");
+mysql_connect("localhost","root","1234");
 mysql_select_db("airclean_Database");
 ?>
